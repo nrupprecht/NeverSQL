2dbcc763375dbdbfc8f599b02fc84045036f6353
Nathaniel Rupprecht <nathaniel.rupprecht@gmail.com>
2024-04-11 08:11:38 -0400

    Remove a GetDataTypeEnum

--- include/NeverSQL/utility/DataTypes.h
+++ include/NeverSQL/utility/DataTypes.h
@@ -66,11 +66,6 @@ inline DataTypeEnum GetDataTypeEnum<int64_t>() {
 }
 
 template<>
-inline DataTypeEnum GetDataTypeEnum<unsigned long>() {
-  return DataTypeEnum::Int64;
-}
-
-template<>
 inline DataTypeEnum GetDataTypeEnum<uint64_t>() {
   return DataTypeEnum::UInt64;
 }
